44640916b1433b4ff4c1c51bae3d05b5067e098c
alistairdevine <alistair.devine98@gmail.com>
2021-07-14 03:52:10 +0100

    Improvement changes
    Added javascript file for adjustable images in panes
    Added service class without logic to fix presisting / state management.

---
+++ StBarnabasHospiceTable/StBarnabasHospice/Client/wwwroot/Scripts/TableJs.js
@@ -0,0 +1,19 @@
+﻿var blazorInterop = blazorInterop || {};
+
+blazorInterop.registerResizeHandler = function (windowPaneObject) {
+    const paneOne = document.querySelector(`div[_bl_5]`);
+    function resizeHandler() {
+        windowPaneObject.invokeMethodAsync("SetWindowPaneSize",
+            {
+                width: paneOne.clientWidth,
+                height: paneOne.clientHeight
+            });
+    };
+
+    //Set up initial values
+    resizeHandler();
+
+    //Reigster event handler
+    window.addEventListener("mousemove", resizeHandler);
+    window.addEventListener("resize", resizeHandler);
+};
