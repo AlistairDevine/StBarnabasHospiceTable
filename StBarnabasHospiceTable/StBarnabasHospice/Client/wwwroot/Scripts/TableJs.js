8a56380fb01a5e53516fe1690228e90732bf0e9e
alistairdevine <alistair.devine98@gmail.com>
2021-07-28 18:36:56 +0100

    Image javascript adjustable working
    Finally got the window pane image javascript solution.
    - Resize observer api
    Ready to configure the output resize values from the api to the image size and repeat for the other five window pane objects that create the table.

--- StBarnabasHospiceTable/StBarnabasHospice/Client/wwwroot/Scripts/TableJs.js
+++ StBarnabasHospiceTable/StBarnabasHospice/Client/wwwroot/Scripts/TableJs.js
@@ -16,13 +16,34 @@ blazorInterop.registerResizeHandler = function (windowPaneObject) {
     //Reigster event handler
     //RESEARCH : DOM events (https://www.w3schools.com/jsref/dom_obj_event.asp)
     window.document.querySelector(`div[_bl_5]`).addEventListener("resize", resizeHandler);
-};
-
-blazorInterop.testPane = function () {
-    const paneOne = document.querySelector(`div[_bl_5]`);
-    return paneOne.width;
 }
 
-//ERROR : event handlers not the cause of the problem
-//ERROR : type of width not the cause of the problem
+blazorInterop.getPaneOneSize = function () {
+    const paneOne = document.querySelector(`div[_bl_5]`);
+    return {
+        width: paneOne.offsetWidth,
+        height: paneOne.offsetHeight
+    };
+}
+
+blazorInterop.registerResizeCallback = function () {
+    //TODO : only event listener triggered when the browser window changes size, not window pane DOM element
+    window.addEventListener("resize", blazorInterop.resized);
+}
+
+blazorInterop.resized = function () {
+    //DotNet.invokeMethod("BrowserResize", 'OnBrowserResize');
+    DotNet.invokeMethodAsync("StBarnabasHospice.Client", 'OnBrowserResize').then(data => data);
+}
+blazorInterop.ApiObserver = function () {
+    const myObserver = new ResizeObserver(entries => {
+        entries.forEach(entry => {
+            console.log('width', entry.contentRect.width);
+            console.log('height', entry.contentRect.height);
+        });
+    });
+
+    const someEl = document.querySelector(`div[_bl_5]`);
+    myObserver.observe(someEl);
+}
 
